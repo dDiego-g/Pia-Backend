a3837d19004f7311515b7784fc54170aeaaca98a
dDiego-g <diego_a981@live.com.mx>
2020-12-11 08:41:51 -0600

    Login

--- src/main/java/com/example/pia/controllers/IndexController.java
+++ src/main/java/com/example/pia/controllers/IndexController.java
@@ -1,25 +1,28 @@
 package com.example.pia.controllers;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @Controller
 @RequestMapping(path = "/app")
 public class IndexController {
-
-	@RequestMapping(path = { "/index", }, method = RequestMethod.GET)
+	
+	@RequestMapping(path = { "/", "","/index"} )
 	public String index(Model model) {
-		model.addAttribute("titulo", "Home Banco");
+	
 		return "index";
 	}
+	
+	@RequestMapping(path = { "/login"} )
+	public String login(Model model) {
+		model.addAttribute("titulo","Login");
+		return "cliente/index";
+	}
+	
 
 }
